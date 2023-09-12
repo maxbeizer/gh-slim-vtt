0fc44ab80d03edd85968deae5fa77f6dd97b2a77
Max Beizer <max.beizer@gmail.com>
2023-09-12 17:23:07 -0500

    Add CLI lib and read the passed in file

--- main.go
+++ main.go
@@ -1,26 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-slim-vtt extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
+	app := &cli.App{
+		Name:  "slim",
+		Usage: "Slim down vtt files",
+		Action: func(cCtx *cli.Context) error {
+			return doIt(cCtx)
+		},
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("running as %s\n", response.Login)
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func doIt(cCtx *cli.Context) error {
+	// open the file at the path given
+	path := cCtx.Args().First()
+	in, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("could not open file: %s", path)
+	}
+	defer func() {
+		if err := in.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Create a new scanner for the file
+	scanner := bufio.NewScanner(in)
+
+	// Read each line of the file
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return nil
+}
